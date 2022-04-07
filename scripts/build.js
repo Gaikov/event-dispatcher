304fd677ef6a619210b4f77137d4d30b77514b2a
CapricornGaikov <gaikov@inboix.ru>
2022-04-07 12:00:39 +0300

    copying sources

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,7 @@
 const fs = require("fs-extra");
 
 const DIST = "./dist";
+const SRC = "src";
 const PACKAGE = "package.json";
 
 function clean() {
@@ -9,8 +10,10 @@ function clean() {
 }
 
 function postBuild() {
-    console.log("...prepare build");
+    console.log("...copying package.json");
     fs.copySync(`./${PACKAGE}`, `${DIST}/${PACKAGE}`, {});
+    console.log("...copying sources");
+    fs.copySync(`./${SRC}`, `${DIST}/${SRC}`, {});
 }
 
 module.exports = {
