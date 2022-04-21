21c7900fab48bb9e6b2c5c9ee4f837520ed5f977
CapricornGaikov <gaikov@inboix.ru>
2022-04-21 12:36:29 +0300

    as common js module

--- scripts/build.js
+++ scripts/build.js
@@ -1,17 +1,22 @@
-import fs from "fs-extra";
+const fs = require("fs-extra");
 
 const DIST = "./dist";
 const SRC = "src";
 const PACKAGE = "package.json";
 
-export function clean() {
+function clean() {
     console.log("...cleaning");
     fs.removeSync("./dist");
 }
 
-export function postBuild() {
+function postBuild() {
     console.log("...copying package.json");
     fs.copySync(`./${PACKAGE}`, `${DIST}/${PACKAGE}`, {});
     console.log("...copying sources");
     fs.copySync(`./${SRC}`, `${DIST}/${SRC}`, {});
+}
+
+module.exports = {
+    clean,
+    postBuild
 }
